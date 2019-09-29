33c5d690cf88a5ba5c578ef0beb4e9b6360e4088
zencloud <mryanmill@gmail.com>
2019-09-28 19:00:46 -0600

    Dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
+import './assets/reset.css';
 import './assets/app_styles.css';
 import CardContainer from './component/CardContainer';
 import Card from './component/Card';
 
 class App extends React.Component {
 
+  // Set game config via state
   state = {
     scoreCurrent: 0,
-    scoreBest:    0,
-    cardArray:    [...Array(9).keys()]
+    scoreBest: 0,
+    cardArray: [...Array(9).keys()],
+    clickedArray: []
   }
 
-  // Handle Input
-  handleClick = (event) => {
-    console.log(event);
+  // Card Click Input
+  handleClick = (id) => {
+
+    if (this.state.clickedArray.includes(id)) {
+      console.log('game over')
+    }
+
+    if (!this.state.clickedArray.includes(id)) {
+
+      this.state.clickedArray.push(id);
+      const newState = {
+        scoreCurrent: this.state.scoreCurrent + 1,
+      }
+
+      this.shuffleCards(this.state.cardArray);
+      this.setState(newState);
+    }
+
+  }
+
+  // Shuffle the card array
+  shuffleCards = (array) => {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+  // Initialize first random shuffle
+  componentDidMount() {
+    this.shuffleCards(this.state.cardArray);
+    this.setState(this.state);
   }
 
   render() {
@@ -22,8 +51,13 @@ class App extends React.Component {
 
         {/* Header */}
         <div className="app-header-container">
-          <h2>Welcome!</h2>
-          <h2>Current Score: {this.state.scoreCurrent}</h2>
+          <div>
+            <h2>Welcome!</h2>
+          </div>
+          <div className="app-score-container">
+            <p>Current Score: {this.state.scoreCurrent}</p>
+            <p>Best Score: {this.state.scoreCurrent}</p>
+          </div>
         </div>
 
         {/* Game Cards */}
