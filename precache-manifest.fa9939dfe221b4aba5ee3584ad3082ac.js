8e47e05233abf08db64bcc15033dcef0c66c2091
zencloud <mryanmill@gmail.com>
2019-09-30 15:39:21 -0600

    Updates

---
+++ precache-manifest.fa9939dfe221b4aba5ee3584ad3082ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f765136345a8feccfb50bb5ce5905df",
+    "url": "/homework_clicky_game/index.html"
+  },
+  {
+    "revision": "ecf52752e4f7019b986e",
+    "url": "/homework_clicky_game/static/css/main.35290af5.chunk.css"
+  },
+  {
+    "revision": "7ba5205732d1f0f1737f",
+    "url": "/homework_clicky_game/static/js/2.b4e0bc9b.chunk.js"
+  },
+  {
+    "revision": "ecf52752e4f7019b986e",
+    "url": "/homework_clicky_game/static/js/main.ec60d839.chunk.js"
+  },
+  {
+    "revision": "9893fc8fcc93485db4fb",
+    "url": "/homework_clicky_game/static/js/runtime-main.45ec8bd2.js"
+  }
+]);
